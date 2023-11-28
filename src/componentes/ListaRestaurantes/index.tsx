234e1a2c1caded66263fe4bdf4addf812765146c
Caio Pfaltzgraff <caio.rllcr@gmail.com>
2023-11-28 18:41:55 -0300

    Paginação tratada

--- src/componentes/ListaRestaurantes/index.tsx
+++ src/componentes/ListaRestaurantes/index.tsx
@@ -3,25 +3,42 @@ import style from './ListaRestaurantes.module.scss';
 import Restaurante from './Restaurante';
 import axios from 'axios';
 import IRestaurante from '../../interfaces/IRestaurante';
+import { IPaginacao } from '../../interfaces/IPaginacao';
 
 const ListaRestaurantes = () => {
   const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([])
+  const [proximaPagina, setProximaPagina] = useState('')
 
   useEffect(() => {
     //obter restaurantes
-    axios.get('http://localhost:8000/api/v1/restaurantes/')
+    axios.get<IPaginacao<IRestaurante>>('http://localhost:8000/api/v1/restaurantes/')
       .then(response => {
         setRestaurantes(response.data.results)
+        setProximaPagina(response.data.next)
       })
       .catch(error => {
         console.log(error)
       })
   }, [])
 
+  const verMais = () => {
+    axios.get<IPaginacao<IRestaurante>>(proximaPagina)
+      .then(response => {
+        setRestaurantes([...restaurantes, ...response.data.results])
+        setProximaPagina(response.data.next)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+
   return (
     <section className={style.ListaRestaurantes}>
       <h1>Os restaurantes mais <em>bacanas</em>!</h1>
       {restaurantes?.map(item => <Restaurante restaurante={item} key={item.id} />)}
+      {proximaPagina && <button onClick={verMais}>
+        Ver mais
+      </button>}
     </section>
   )
 }
