46c47c6ec7f2a7a50eadb546f7c74cd8ed4cfc49
Caio Pfaltzgraff <caio.rllcr@gmail.com>
2023-11-30 14:48:45 -0300

    Enviando pratos a api

--- src/paginas/Administracao/Pratos/FormularioPratos.tsx
+++ src/paginas/Administracao/Pratos/FormularioPratos.tsx
@@ -33,6 +33,33 @@ const FormularioPrato = () => {
     const aoSubmeterForm = (evento: React.FormEvent<HTMLFormElement>) => {
         evento.preventDefault();
 
+        const formData = new FormData();
+
+        formData.append('nome', nomePrato)
+        formData.append('descricao', descricao);
+        formData.append('tag', tag);
+        formData.append('restaurante', restaurante);
+        if(imagem) {
+            formData.append('imagem', imagem);
+        }
+
+        http.request({
+            url: 'pratos/',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            },
+            data: formData
+        })
+            .then(() => {
+                setNomePrato('')
+                setDescricao('')
+                setTag('')
+                setRestaurante('')
+                setImagem(null)
+                alert('Prato cadastrado com sucesso!')
+            })
+            .catch((erro) => console.log(erro))
     } 
 
     return(
@@ -62,7 +89,7 @@ const FormularioPrato = () => {
                     <FormControl margin="dense" fullWidth >
                         <InputLabel id="select-tag">Tag</InputLabel>
                         <Select labelId="select-tag" value={tag} onChange={e => setTag(e.target.value)}>
-                            {tags.map(tag => <MenuItem value={tag.id} key={tag.id}>
+                            {tags.map(tag => <MenuItem value={tag.value} key={tag.id}>
                                 {tag.value}
                             </MenuItem>)}
                         </Select>
